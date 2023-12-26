8576d9d365e651abcd5b42bb58ade458b058dfca
vporel <dev.vporel@gmail.com>
2023-12-26 09:36:57 +0100

    build

---
+++ src/index.js
@@ -0,0 +1,18 @@
+/* Classes */
+import FileUploader from './classes/FileUploader'; 
+const classes = {FileUploader}
+export {classes};
+
+import * as api from './api'; export {api};
+import * as array from './array'; export {array};
+import * as date from './date'; export {date};
+import * as dom from './dom'; export {dom};
+import * as form from './form'; export {form};
+import * as numbers from './numbers'; export {numbers};
+import * as photoviewer from './photoviewer'; export {photoviewer};
+import * as propTypes from './prop-types'; export {propTypes};
+import * as serviceWorker from './service-worker'; export {serviceWorker};
+import * as standard from './standard'; export {standard};
+import * as strings from './strings'; export {strings};
+import * as translator from './translator'; export {translator};
+import * as validators from './validators'; export {validators};
