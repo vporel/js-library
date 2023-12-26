0fb7a69c5d18a8c7903053f30e46c50cc8860db9
vporel <dev.vporel@gmail.com>
2023-12-26 08:33:17 +0100

    first commit

--- components/display/FlexCenter.jsx
+++
@@ -1,7 +0,0 @@
-import { classNames } from "../../dom";
-
-export default function FlexCenter({className, children, ...props}){
-    return <div className={classNames("d-flex justify-content-center align-items-center gap-2", className)} {...props}>
-        {children}
-    </div>
-}
